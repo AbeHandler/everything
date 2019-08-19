662853117070345cf62552c7d3a8dc47e4366dbf
Abe Handler <abram.handler@gmail.com>
2019-08-19 14:41:52 -0400

    updates

--- code/loglin.py
+++ code/loglin.py
@@ -2,12 +2,13 @@ import numpy as np
 import copy
 import pickle
 
+from functools import lru_cache
 from tqdm import tqdm_notebook as tqdm
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.special import logsumexp
 # http://cs.jhu.edu/~jason/tutorials/loglin/formulas.pdf
 
-@lru_cache(maxsize=100000)
+@lru_cache(maxsize=1000000)
 def phi(n_i, e, z):    
     '''One feature needs to be the PMI of n_i'''
 
@@ -42,9 +43,9 @@ def fprime(w, *args):
     
     This can handle weighted examples via the point_weights argument
     '''
-    dataset, Y, feats, actual_feats, true_feature_counts, point_weights = args
+    dataset, Y, feats, actual_feats, point_weights = args
     expected_feature_counts = get_expected_feats_for_dataset(dataset, w, feats)
-    delta = np.sum(point_weights * true_feature_counts, axis=0) - np.sum(point_weights * expected_feature_counts, axis=0)
+    delta = np.sum(point_weights * actual_feats, axis=0) - np.sum(point_weights * expected_feature_counts, axis=0)
     return -1 * delta
 
 
@@ -78,7 +79,7 @@ def get_log_probs(w, *args):
     This can handle weighted examples via the point_weights param
     '''
     
-    dataset, Y, feats, actual_feats, true_feature_counts, point_weights = args
+    dataset, Y, feats, actual_feats, point_weights = args
     
     n = actual_feats.dot(w)
     
@@ -103,31 +104,32 @@ def make_all_feats(data_set):
     
     return feats, actual_feats
 
-d0 = [("Ari", "Ariana", 0), 
+
+if __name__ == "__main__":
+   
+
+    d0 = [("Ari", "Ariana", 1), 
       ("bill", "Ariana", 0), 
       ("bill", "Rihanna", 0),
-      ("Riri", "Rihanna", 0),
-      ("sand", "Ariana", 0)]
-
-
-d1 = [("Ari", "Ariana", 1),  
-      ("bill", "Ariana", 1), 
-      ("bill", "Rihanna", 1),
       ("Riri", "Rihanna", 1),
-      ("sand", "Ariana", 1)]
+      ("sand", "Ariana", 0)] 
 
-w = np.asarray([.2, .14, .32, .3])
 
-feats, actual_feats = make_all_feats(d1)
+    print("making feats")
+            
+    w = np.asarray([.2, .14, .32, .3])
 
-true_feature_counts = np.sum(actual_feats, axis=0)
+    feats, actual_feats = make_all_feats(d0)
+    
+    point_weights = np.ones((len(d0), 1))
+    
+    Y = set([_[0] for _ in d0])
+    
+    assert Y == set([o[0] for o in d0])
+    
+    print("ok")
+    print(np.exp(get_log_probs(w, d0, Y, feats, actual_feats, point_weights)))
 
-Y = set([_[0] for _ in d1])
+    w, f, d = fmin_l_bfgs_b(func=get_log_probs, fprime=fprime, x0=w, args=(d0, Y, feats, actual_feats, point_weights))
 
-print(np.exp(get_log_probs(w, dataset, Y, feats, actual_feats, true_feature_counts)))
-
-w, f, d = fmin_l_bfgs_b(func=get_log_probs, fprime=fprime, x0=w, args=(dataset, Y, feats, actual_feats, true_feature_counts))
-
-print(np.exp(get_log_probs(w, dataset, Y, feats, actual_feats, true_feature_counts)))
-
-get_probs(w, dataset, Y, feats, actual_feats)
+    print(np.exp(get_log_probs(w, d0, Y, feats, actual_feats, point_weights)))
