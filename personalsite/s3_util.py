0723c6a5130fac5a8ceba4f71158bf5d37271731
Abe Handler <akh2103@gmail.com>
2019-09-07 14:27:36 -0400

    just backup

--- personalsite/s3_util.py
+++ personalsite/s3_util.py
@@ -3,5 +3,8 @@ import sys
 with open("upload.sh", "w") as of:
     for line in sys.stdin:
         out = "aws s3 cp " + line.strip("\n") + " " + "s3://www.abehandler.com/" + line.replace("_site/", "").replace("\n", "") + " --acl public-read --content-type text/html"
-        print out
+        print(out)
         of.write(out + "\n")
+    of.write("aws s3 cp images s3://www.abehandler.com/images --recursive  --acl public-read" + "\n")
+    of.write("aws s3 cp _site/index.html s3://www.abehandler.com/index.html --acl public-read" + "\n")
+    of.write('aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"' + "\n")
