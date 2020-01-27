b379d7efdd351ec93f21e9fd759a9d6c110b722e
Abe Handler <akh2103@gmail.com>
2020-01-27 11:49:46 -0500

    just backup

---
+++ personalsite/upload.sh
@@ -0,0 +1,6 @@
+aws s3 cp _site/mcmc/2019/11/01/why-does-metropolis-work.html s3://www.abehandler.com/mcmc/2019/11/01/why-does-metropolis-work.html --acl public-read --content-type text/html
+aws s3 cp _site/em/2019/10/01/implementing-em-without-tears.html s3://www.abehandler.com/em/2019/10/01/implementing-em-without-tears.html --acl public-read --content-type text/html
+aws s3 cp images s3://www.abehandler.com/images --recursive  --acl public-read
+aws s3 cp _site/index.html s3://www.abehandler.com/index.html --acl public-read
+aws s3 cp _site/blog.html s3://www.abehandler.com/blog.html --acl public-read
+aws cloudfront create-invalidation --distribution-id E2NDQN6OXXN3XW --paths "/*"
