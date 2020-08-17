8a5352fec540b92ecd5124139c283179fb96173c
Abe Handler <akh2103@gmail.com>
2020-08-17 08:03:09 -0600

    just backup

--- teaching/4604fall2020/data/get_data.R
+++ teaching/4604fall2020/data/get_data.R
@@ -7,6 +7,14 @@ pop %>% left_join(count, by = c("state/region" = "state"))
 
 z <- pop %>% left_join(count, by = c("state/region" = "state")) %>% filter(year=="2013")
 
+system("wget https://raw.githubusercontent.com/CivilServiceUSA/us-governors/master/us-governors/data/us-governors.csv  -O gov.csv")
+
+gov <- read_csv("gov.csv") %>% select("state_code", "party")
+
 final <- z %>% select(`state/region`, population, positive)
 
-final
+with_gov = final %>% left_join(gov, by = c("state/region" = "state_code"))
+
+p <- ggplot(with_gov) + geom_point(aes(x=population, y=positive, color = `party`))
+
+p <- p + geom_text(aes(x=population, y=positive, label = `state/region`)) + xlim(0, 39332521)
