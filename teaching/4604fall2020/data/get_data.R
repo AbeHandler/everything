50dd488ca6439a4f6052739f4668e707cc11c4e5
Abe Handler <akh2103@gmail.com>
2020-08-20 20:49:36 -0600

    just backup

--- teaching/4604fall2020/data/get_data.R
+++ teaching/4604fall2020/data/get_data.R
@@ -13,7 +13,7 @@ gov <- read_csv("gov.csv") %>% select("state_code", "party")
 
 final <- z %>% select(`state/region`, population, positive)
 
-with_gov = final %>% left_join(gov, by = c("state/region" = "state_code"))
+with_gov = final %>% left_join(gov, by = c("state/region" = "state_code")) %>% filter(`state/region` != "USA")
 
 big_max = 40000000
 small_max = 10000000
@@ -21,3 +21,7 @@ small_max = 10000000
 p <- ggplot(with_gov) + geom_point(aes(x=population, y=positive, color = `party`)) + xlim(0, big_max)
 
 p <-p + geom_text(aes(x=population, y=positive, label = `state/region`), hjust = -.5)
+
+
+
+write_csv(with_gov, "out.csv")
