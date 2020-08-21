ceb88bb4df968eb8f2e4df1166c0d2832da5a229
Abe Handler <akh2103@gmail.com>
2020-08-21 11:25:01 -0600

    just backup

--- teaching/scripts/canvas_api.py
+++ teaching/scripts/canvas_api.py
@@ -1,5 +1,5 @@
 '''
-Proof of concept for accessing the canvas API
+Abe Handler's scripts for accessing the canvas API
 
 1. Go to https://canvas.colorado.edu/profile/settings and click "+ New Access Token"
 2. in your local shell, run export CANVAS_TOKEN="my_secret_token"
@@ -32,9 +32,13 @@ def createInClassAssignment(courseNo, date):
 
     date = datetime.strptime(date, '%Y%m%d')
 
+    print("[*] Creating in-class assignment {} for {}".format(courseNo, date))
+
     new_assignment = course.create_assignment({
         'name': 'In-class assignment, {}'.format(date.strftime("%b %d")),
         'published': True,
+        'unlock_at': date.strftime('%Y-%m-%d') + "T09:00:00",
+        'lock_at': date.strftime('%Y-%m-%d') + "T17:00:00",
         "assignment_group_id": group_id
     })
 
@@ -48,8 +52,7 @@ if __name__ == "__main__":
     # map course to in-class assignment groups
     COURSE2INCLASS = {"4604": "149100"}
 
-    course = canvas.get_course(COURSES["4604"])
-
+    # course = canvas.get_course(COURSES["4604"])
     # print(course.name)
     # course.create_quiz({"title": "test"})
 
