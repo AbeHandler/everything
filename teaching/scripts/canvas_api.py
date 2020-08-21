a80b7905b5e1272897f3581b31f8866129d54789
Abe Handler <akh2103@gmail.com>
2020-08-21 11:01:27 -0600

    just backup

--- teaching/scripts/canvas_api.py
+++ teaching/scripts/canvas_api.py
@@ -9,16 +9,48 @@ Proof of concept for accessing the canvas API
 
 # Import the Canvas class
 import os
+from datetime import datetime
 from canvasapi import Canvas
 
-# Canvas API URL
-API_URL = "https://canvas.colorado.edu"
-# Canvas API key
-API_KEY = os.environ['CANVAS_TOKEN']
 
-# Initialize a new Canvas object
-canvas = Canvas(API_URL, API_KEY)
+def get_api():
 
-course = canvas.get_course(62561)
+    # Canvas API URL
+    API_URL = "https://canvas.colorado.edu"
+    # Canvas API key
+    API_KEY = os.environ['CANVAS_TOKEN']
 
-print(course.name)
+    # Initialize a new Canvas object
+    return Canvas(API_URL, API_KEY)
+
+
+def createInClassAssignment(courseNo, date):
+
+    course = canvas.get_course(COURSES[courseNo])
+
+    group_id = COURSE2INCLASS[courseNo]
+
+    date = datetime.strptime(date, '%Y%m%d')
+
+    new_assignment = course.create_assignment({
+        'name': 'In-class assignment, {}'.format(date.strftime("%b %d")),
+        'published': True,
+        "assignment_group_id": group_id
+    })
+
+
+if __name__ == "__main__":
+    canvas = get_api()
+
+    # map CU names to names in Canvas
+    COURSES = {"4604": 62561}
+
+    # map course to in-class assignment groups
+    COURSE2INCLASS = {"4604": "149100"}
+
+    course = canvas.get_course(COURSES["4604"])
+
+    # print(course.name)
+    # course.create_quiz({"title": "test"})
+
+    createInClassAssignment(courseNo="4604", date="20200826")
