8033790181b1b20a42e09b697bfc3d9d81cb9907
Abe Handler <akh2103@gmail.com>
2020-08-23 06:32:55 -0600

    just backup

--- teaching/scripts/canvas_cli.py
+++ teaching/scripts/canvas_cli.py
@@ -1,10 +1,18 @@
 '''
-Abe Handler's command-line scripts for accessing the canvas API
+Command-line interface to the canvas API.
+- This will modify your canvas courses
+- Start with your sandbox course when learning
+- Mostly just a wrapper over https://github.com/ucfopen/canvasapi
 
-Setup:
+Setup for CU users:
 1. Go to https://canvas.colorado.edu/profile/settings and click "+ New Access Token"
 2. in your local shell, run export CANVAS_TOKEN="my_secret_token"
 3. Install the python client $pip install canvasapi
+
+Questions and contact:
+abram.handler@gmail.com
+www.abehandler.com
+
 '''
 
 
@@ -25,7 +33,7 @@ def get_api():
     return Canvas(API_URL, API_KEY)
 
 
-def createInClassAssignment(courseNo, date):
+def createInClassAssignment(courseNo, date, published=False):
 
     course = canvas.get_course(COURSES[courseNo])
 
@@ -37,7 +45,7 @@ def createInClassAssignment(courseNo, date):
 
     course.create_assignment({
         'name': 'In-class assignment, {}'.format(date.strftime("%b %d")),
-        'published': True,
+        'published': published,
         'unlock_at': date.strftime('%Y-%m-%d') + "T09:00:00",
         'lock_at': date.strftime('%Y-%m-%d') + "T17:00:00",
         "assignment_group_id": group_id,
@@ -51,20 +59,32 @@ if __name__ == "__main__":
     canvas = get_api()
 
     # map CU names to names in Canvas
-    COURSES = {"4604": 62561}
+    COURSES = {"4604": 62561, "sandbox": 62535}
 
     # map course to in-class assignment groups
     COURSE2INCLASS = {"4604": "149100"}
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--course', help='INFO course number, e.g. 4604')
 
-    parser.add_argument('--inClass', help='pass a date in YYYYMMDD to create in-class assignment, e.g. 20200824')
+    parser.add_argument('--course', default='sandbox', help='INFO course number, e.g. 4604')
+
+    parser.add_argument('--quiz', '-quiz', dest='quiz', default='false', action='store_true', help='Use this flag to create a quiz')
+
+    parser.add_argument('--assignment', '-assignment', dest='assignment', default='false', action='store_true', help='Use this flag to create an assignment')
+
+    parser.add_argument('--due', help='pass a date in YYYYMMDD for the due date, e.g. 20200824')
+
+    parser.add_argument('--publish', dest='publish', default='false', action='store_true', help='Use this flag to immediately publish the assignment')
 
     args = parser.parse_args()
 
+    if(args.quiz):
+        course = canvas.get_course(COURSES[args.course])
+        course.create_quiz({'title': "test"})
+    '''
     try:
-        datetime.strptime(args.inClass, '%Y%m%d')
-        createInClassAssignment(courseNo=args.course, date=args.inClass)
+        datetime.strptime(args.dueDate, '%Y%m%d')
+        createInClassAssignment(courseNo=args.course, date=args.dueDate)
     except ValueError:
         print("[*] The argument inClass needs to match the format YYYYMMDD. Won't make assignment.")
+    '''
