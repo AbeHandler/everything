613d4006de3a7f19ed302b72381b7df1d0a39c1d
Abram Handler <abramhandler@Abrams-MacBook-Pro.local>
2020-09-01 20:09:41 -0600

    ok

---
+++ teaching/2301fall2020/week2/assignment_files/setops.py
@@ -0,0 +1,36 @@
+def times_three(n):
+    '''return an integer equal to n times 3'''
+    return n * 3
+
+
+def cardinality(input_set):
+    '''return an integer equal to the cardinality of the input_set'''
+    # hint: use a for loop
+
+    # implement me!
+    return 0
+
+
+def set_difference(A, B):
+    '''
+    Bonus question:
+        Return all elements of A that are not in B in the variable called output
+
+    If you attempt this question, upload your setops.py file to the week2 bonus question
+    '''
+
+    output = set()
+
+    # implement me. You need to add elements to the set output
+
+    return output
+
+
+def subset(A, B):
+    '''
+    return True if A is a subset of B, False if A is not a subset of B
+    '''
+
+    # implement me
+
+    return False  #  you should change this line
