7c3790d7195d15c69569b19d028b288c4ebbd591
Abram Handler <abramhandler@Abrams-MBP.Home>
2020-09-07 15:06:49 -0600

    ok

---
+++ teaching/2301fall2020/week3/assignment_files/vectors_functions.py
@@ -0,0 +1,18 @@
+def sigma_sq(N):
+    '''
+    returns \Sigma_i^N x_i * x_i
+	- You will need to double click the cells in the note book to understand this notation
+	- You can copy and paste it into a markdown cell to see it in a nicer-looking format
+    '''
+    output = 0
+    return output
+
+
+def sigma_2n(N):
+    '''
+    returns \Sigma_i^N x_i * 2
+	- You will need to double click the cells in the note book to understand this notation
+    '''
+    output = 0 
+    print("Please fill out this function")
+    return output
