44c8fd892dc08bb6ff4eef7d8f3950966a798edf
Abram Handler <abramhandler@Abrams-MBP.Home>
2020-09-12 14:13:14 -0600

    ok

---
+++ teaching/2301fall2020/week4/assignment_files/vectors2.py
@@ -0,0 +1,14 @@
+def normalize(a):
+     '''
+     Return the vector a (i.e. a list) normalized, so that the magnitude is 1
+     '''
+     pass
+
+
+def dot_product(a, b):
+     '''
+     return the dot product of a and b, i.e. 
+
+     \Sigma a_i * b_i
+     '''
+     pass
