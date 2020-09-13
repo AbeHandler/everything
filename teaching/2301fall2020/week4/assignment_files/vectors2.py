fff5f65831069e1fb6f9dec02941ab9a0f667b02
Abram Handler <abramhandler@Abrams-MBP.Home>
2020-09-13 09:37:14 -0600

    ok

--- teaching/2301fall2020/week4/assignment_files/vectors2.py
+++ teaching/2301fall2020/week4/assignment_files/vectors2.py
@@ -1,3 +1,10 @@
+def add_two_numbers(a,b):
+     return sum 
+     sum = sum + a
+     sum = 0
+     sum = sum + b
+
+
 def normalize(a):
      '''
      Return the vector a (i.e. a list) normalized, so that the magnitude is 1
