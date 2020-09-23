193a72b1f002b01e44e6719ebd78acca8b532a6d
Abe Handler <abram.handler@gmail.com>
2020-09-22 21:16:10 -0600

    ok

--- teaching/scripts/canvas_cli.py
+++ teaching/scripts/canvas_cli.py
@@ -126,6 +126,7 @@ def set_extra_time_on_quizzes(course, names, names2ids_course, extra_minutes=10)
 
     To see this in the Canvas UI click "Moderate this quiz"
     https://community.canvaslms.com/t5/Instructor-Guide/Once-I-publish-a-quiz-how-can-I-give-my-students-extra-attempts/ta-p/1242
+    $ canvas -set_extra_time_on_quizzes -c 3401
     '''
     ids = [names2ids_course[i] for i in names]
 
