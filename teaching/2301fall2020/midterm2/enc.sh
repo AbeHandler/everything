77e8dad3882ae5bcec5e16b04ada5eddf61d93b0
Abe Handler <abram.handler@gmail.com>
2020-11-14 18:33:56 -0700

    oka

--- teaching/2301fall2020/midterm2/enc.sh
+++ teaching/2301fall2020/midterm2/enc.sh
@@ -1,1 +1,4 @@
+# On machine: ln -s /Users/abramhandler/.enc.sh ~/bin/enc.sh
+# in zshrc: alias enc="enc.sh"
+
 openssl enc -aes-256-cbc -salt -in $1 -out $1.enc -iter 10000 -pass file:$HOME/.ssh/id_rsa 
