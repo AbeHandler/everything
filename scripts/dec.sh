35553eb325db13bbbce5e35cfdf30d0d2f7a0ff0
Abe Handler <abram.handler@gmail.com>
2020-11-14 18:36:52 -0700

    ok

--- scripts/dec.sh
+++ scripts/dec.sh
@@ -1,2 +1,4 @@
-echo $1
+# On machine: ln -s /Users/abramhandler/.dec.sh ~/bin/dec.sh
+# in zshrc: alias dec="dec.sh"
+echo "Attempting to decrypt $1"
 openssl enc -d -aes-256-cbc -in $1.enc -out Midterm2_answers.ipynb -iter 10000 -pass file:$HOME/.ssh/id_rsa
