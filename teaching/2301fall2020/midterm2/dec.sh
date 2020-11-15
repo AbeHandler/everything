cf0ab8a6ade7fb1db2bb67e6b19f6290b7a0f7d2
Abe Handler <abram.handler@gmail.com>
2020-11-14 17:23:57 -0700

    oik

--- teaching/2301fall2020/midterm2/dec.sh
+++ teaching/2301fall2020/midterm2/dec.sh
@@ -1,1 +1,2 @@
-openssl enc -d -aes-256-cbc -in Midterm2_answers.ipynb.enc -out Midterm2_answers.ipynb -iter 10000
+echo $1
+openssl enc -d -aes-256-cbc -in $1.enc -out Midterm2_answers.ipynb -iter 10000 -pass file:/Users/abramhandler/.ssh/id_rsa
