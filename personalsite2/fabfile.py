1eab09d941fc1111eeef7cb9c46ead67917e73c1
Abe Handler <abram.handler@gmail.com>
2020-11-25 19:53:44 -0700

    ok

---
+++ personalsite2/fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import *
+
+def build():
+    local("bundle exec jekyll build")
+
+def serve():
+    local("bundle exec jekyll serve")
+
+def deploy():
+    build()
+    local("cat _site/blog.html |  grep href | grep h3 | awk -F'\"' '{print \"_site\"$2}' | python s3_util.py") # writes to upload.sh
+    local("chmod +x upload.sh && ./upload.sh")
+    local("aws s3 cp _site/css/style.css s3://www.abehandler.com/css/style.css --acl public-read")
+    local("git push origin main")
