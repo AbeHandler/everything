76ab10acfacc5d1da1cc019a7930eaf010bfc009
Abe Handler <abram.handler@gmail.com>
2021-05-02 08:16:27 -0600

    just backup

--- teaching/scripts/histogram.R
+++ teaching/scripts/histogram.R
@@ -1,6 +1,5 @@
 a <- read_csv("2021-05-02T0714_Grades-INFO_2301-001.csv")
 b <- a["Final (1036622)"]
 b <- b %>% rename("final" = "Final (1036622)") # syntax is new=old
-ggplot(b, aes(x=final)) + geom_histogram(bins=15)  # no quote on column name
-
-summary(b)
+ggplot(b, aes(x=final)) + geom_histogram(bins=15)  + ggtitle("final scores") # no quote on column name
+ggsave("final_scores.jpg")
