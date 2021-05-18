4e3b5f190715089bb4d0d190d6f63e73be8485ea
Abe Handler <abram.handler@gmail.com>
2021-05-18 14:17:02 -0600

    ok

--- scratch/stan_learning.R
+++ scratch/stan_learning.R
@@ -42,6 +42,54 @@ model {
 }
 "
 
+incorrect_model <- "
+data {
+  int N; // number of observations
+  vector[N] y; //outcome vector
+}
+
+parameters {
+  real mu; 
+  real sigma;
+}
+
+model {
+
+  //prior
+  mu ~ normal(0, 5);
+  sigma ~ normal(0, 10);
+  
+  // The likelihood
+  y ~ normal(mu, sigma);
+
+}
+generated quantities {
+  // For model comparison, we'll want to keep the likelihood contribution of each point
+  // We will also generate posterior predictive draws (yhat) for each data point. These will be elaborated on below. 
+  
+  vector[N] log_lik;
+  vector[N] y_sim;
+  for(i in 1:N){
+    log_lik[i] = normal_log(y[i], mu, sigma);
+    y_sim[i] = normal_rng(mu, sigma);
+  }
+}
+"
+
 compiled_function <- stan_model(model_code = dgp_string)
 
 expose_stan_functions(compiled_function)
+
+mus = c(-10, 10)
+
+N = 1000
+
+y_sim = sample_normal_rng(mus, theta, N)
+
+df = data_frame(y_sim = y_sim)
+
+p <- ggplot(df, aes( x = y_sim)) + geom_histogram(bins=30) + ggtitle("Simulated data")
+
+data_list_2 <- list(N = N, y = y_sim)
+
+incorrect_fit <- stan(model_code = incorrect_model, data = data_list_2, cores = 1, chains = 2, iter = 2000)
