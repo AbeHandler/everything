462ab234fd3f48be573d1a3781a78637e14c6263
Abe Handler <abram.handler@gmail.com>
2021-05-18 15:02:14 -0600

    ok

--- scratch/stan_learning.R
+++ scratch/stan_learning.R
@@ -57,10 +57,10 @@ model {
 
   //prior
   mu ~ normal(0, 5);
-  sigma ~ normal(0, 10);
+  sigma ~ normal_lpdf(0, 10);
   
   // The likelihood
-  y ~ normal(mu, sigma);
+  y ~ normal_lpdf(mu, sigma);
 
 }
 generated quantities {
@@ -70,8 +70,55 @@ generated quantities {
   vector[N] log_lik;
   vector[N] y_sim;
   for(i in 1:N){
-    log_lik[i] = normal_log(y[i], mu, sigma);
+    log_lik[i] = normal_lpdf(y[i] | mu, sigma);
     y_sim[i] = normal_rng(mu, sigma);
+  }
+}
+"
+
+correct_model <- "
+data {
+  int N; // number of observations
+  vector[N] y; //outcome vector
+}
+
+parameters {
+  ordered[2] mu;
+  real<lower=0> sigma[2];
+  real<lower=0, upper=1> theta;
+}
+
+model {
+
+ sigma ~ normal(0, 2);
+ mu ~ normal(0, 2);
+ theta ~ beta(5, 5);
+ for (n in 1:N)
+   target += log_mix(theta,
+                     normal_lpdf(y[n] | mu[1], sigma[1]),
+                     normal_lpdf(y[n] | mu[2], sigma[2]));
+
+}
+generated quantities {
+  // For model comparison, we'll want to keep the likelihood contribution of each point
+  // We will also generate posterior predictive draws (yhat) for each data point. These will be elaborated on below. 
+  
+  vector[N] log_lik;
+  vector[N] y_sim;
+  int z; // TODO hard coded 2-positions
+  for(i in 1:N){
+  
+    // sample. Not sure if better way
+    z = bernoulli_rng(theta); 
+    
+
+    log_lik[i] = log_mix(theta,
+                        normal_lpdf(y[i] | mu[1], sigma[1]),
+                        normal_lpdf(y[i] | mu[2], sigma[2]));
+
+
+    y_sim[i] = normal_rng(mu[z + 1], 1.0);
+
   }
 }
 "
@@ -82,7 +129,7 @@ expose_stan_functions(compiled_function)
 
 mus = c(-10, 10)
 
-N = 1000
+N = 100
 
 y_sim = sample_normal_rng(mus, theta, N)
 
@@ -93,3 +140,5 @@ p <- ggplot(df, aes( x = y_sim)) + geom_histogram(bins=30) + ggtitle("Simulated 
 data_list_2 <- list(N = N, y = y_sim)
 
 incorrect_fit <- stan(model_code = incorrect_model, data = data_list_2, cores = 1, chains = 2, iter = 2000)
+
+correct_fit <- stan(model_code = correct_model, data = data_list_2, cores = 1, chains = 2, iter = 2000)
